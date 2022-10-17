6990926c330f7c655096908381a71360e03bdd22
Shapoval <olekcandrsapoval41@gmail.com>
2022-10-17 20:59:21 +0300

    Home_Work_09_10

---
+++ main.py
@@ -0,0 +1,30 @@
+#Завдання 1
+print("Ведіть число e:")
+e = int (input())
+print("Ведіть число x:")
+x = int (input())
+print("Ведіть число y:")
+y = int (input())
+w = x+y+e
+r = x*y*e
+print("Сума чисел:", w ,"Добуток чисел", r)
+#Завдання 2
+print("Ваша зарплата:")
+x = int (input())
+print("Сума місячного платежу за кредитом банку:")
+y = int (input())
+print("Ваша заборгованість за комунальні послуги:")
+e = int (input())
+w = x - y - e
+print("Залишок:", w )
+#Завдання 3
+print("Діагональ 1:")
+x = int (input())
+print("Діагональ 2:")
+y = int (input())
+w = x*y/2
+print("Площа", w )
+#Завдання 4
+print('To be\nor not\nto be')
+#Завдання 5
+print('Life is what happens\n\twhen\n\t\tyou’re busy making other plans.\n\t\t\t\t\t\t\tJohn Lennon')
